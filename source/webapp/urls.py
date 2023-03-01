411bd9d3934f96db7ab906bae6fd47f2e4530bc6
Akramerwin <akram.beat77@gmail.com>
2023-03-01 23:34:47 +0600

    Пайтон пакет

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import file_upload
+from .views import file_upload, IndexView
 
 app_name = 'webapp'
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
     path('file_upload/', file_upload , name='file_upload')
 ]
--- source/webapp/views/views.py
+++ source/webapp/views/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .handle_upload import handle_uploaded_file
-from .models import File
+from webapp.handle_upload import handle_uploaded_file
+from webapp.models import File
 from webapp.forms import FileForm
+from django.urls import reverse_lazy, reverse
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
+class IndexView(TemplateView):
+    template_name = 'index.html'
 
 # Create your views here.
 
