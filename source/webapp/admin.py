6eaedd092e67c5595dfdf25cc3a83f533c7df8db
CityNomad <romancheva@hotmail.com>
2023-03-02 00:20:15 +0600

    Parsing has been added/ Добавлен парсинг

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from accounts.models import User
-from webapp.models import Country, City, Club, Game, Tournament, Player, PlayerInTournament, Recommendation
+from .models import Country, City, Club, Game, Tournament, Player, PlayerInTournament
 
 admin.site.register(User)
 admin.site.register(Country)
@@ -10,4 +10,4 @@ admin.site.register(Game)
 admin.site.register(Tournament)
 admin.site.register(Player)
 admin.site.register(PlayerInTournament)
-admin.site.register(Recommendation)
+
