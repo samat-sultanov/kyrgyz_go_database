b2ef5b540a8d6dcbb70067eb110cb65100b618f0
Daniiar_Abdukarimov <d.abdukarimov500@gmail.com>
2023-03-07 01:13:05 +0600

    Добавлены поля лого и город в модель Club, отрефакторен шаблон, добавлена пагинация. Вывод побед и поражений, а также сортировки в процессе. Добавлена папка club_logo в uploads.

--- source/webapp/views/club_view.py
+++ source/webapp/views/club_view.py
@@ -1,4 +1,4 @@
-from django.db.models import Count
+from django.db.models import Avg, Count
 
 from webapp.models import Club
 from django.views.generic import ListView
@@ -8,4 +8,6 @@ class ClubsListView(ListView):
     model = Club
     template_name = 'club/club_list.html'
     context_object_name = 'clubs'
+    paginate_by = 15
+    paginate_orphans = 4
 
