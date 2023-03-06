08139cbe9c973e321681851be3e21625b006163e
Dastan T <dastan.turatbekov@gmail.com>
2023-03-06 17:29:55 +0600

    kgf-78/Добавил представление

--- source/webapp/views/news_views.py
+++ source/webapp/views/news_views.py
@@ -1,6 +1,9 @@
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.views.generic import ListView, CreateView
 
 from webapp.models import News
+from webapp.forms import NewsForm
 
 
 # Этот класс написан Акрамом. Я - Дастан, перенес его в этот файл из views.py
@@ -9,3 +12,18 @@ class NewsListView(ListView):
     template_name = 'news/news_list.html'
     context_object_name = 'news_list'
     ordering = ['-created_at']
+
+
+class NewsCreateView(LoginRequiredMixin, CreateView):
+    model = News
+    form_class = NewsForm
+    template_name = 'news/news_create.html'
+    context_object_name = 'news'
+
+    # Когда будет готово представление и шаблон детального просмотра статьи, надо будет success_url поменять
+    def get_success_url(self):
+        return reverse('webapp:index')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
