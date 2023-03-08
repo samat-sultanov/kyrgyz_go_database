fe9041f8a22355e92f8e421692cefe99d46e7b04
CityNomad <romancheva@hotmail.com>
2023-03-09 00:40:37 +0600

    Выведены топ-3 игрока федерации игры в Го на главной странице, feature/KGF-85

--- source/webapp/views/player_view.py
+++ source/webapp/views/player_view.py
@@ -9,6 +9,48 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 
+def get_position_in_kgf():
+    country = Country.objects.get(country_code='kg')
+    players = Player.objects.filter(country=country)
+    tournaments = Tournament.objects.order_by("date")
+    new_list = []
+    for player in players:
+        new_dict = dict()
+        for tournament in tournaments:
+            for data in tournament.playerintournament_set.all():
+                if player.pk == data.player_id:
+                    if player.pk not in new_dict:
+                        new_dict['player'] = player.pk
+                        p = re.compile('(\d*)')
+                        m = p.findall(data.GoLevel)
+                        for i in m:
+                            if i != "":
+                                new_dict['GoLevel'] = int(i)
+        new_list.append(new_dict)
+    new_list.sort(key=lambda dictionary: dictionary['GoLevel'])
+    position = 1
+    for element in new_list:
+        element['position'] = position
+        position += 1
+    return new_list
+
+
+def get_rank():
+    players = Player.objects.all()
+    tournaments = Tournament.objects.order_by("date")
+    new_list = []
+    for player in players:
+        new_dict = dict()
+        for tournament in tournaments:
+            for data in tournament.playerintournament_set.all():
+                if player.pk == data.player_id:
+                    if player not in new_dict:
+                        new_dict['player'] = player.pk
+                        new_dict['GoLevel'] = data.GoLevel
+        new_list.append(new_dict)
+    return new_list
+
+
 class PlayerDetail(TemplateView):
     context_key = 'player'
     key_kwarg = 'pk'
@@ -18,7 +60,7 @@ class PlayerDetail(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context[self.context_key] = self.get_object()
-        context['position'] = self.get_position_in_kgf()
+        context['position'] = get_position_in_kgf()
         context['rank'] = self.get_rank()
         return context
 
@@ -36,32 +78,6 @@ class PlayerDetail(TemplateView):
                 new_dict['player'] = player.pk
                 new_dict['GoLevel'] = data.GoLevel
         new_list.append(new_dict)
-        return new_list
-
-    @staticmethod
-    def get_position_in_kgf():
-        country = Country.objects.get(country_code='kg')
-        players = Player.objects.filter(country=country)
-        tournaments = Tournament.objects.order_by("date")
-        new_list = []
-        for player in players:
-            new_dict = dict()
-            for tournament in tournaments:
-                for data in tournament.playerintournament_set.all():
-                    if player.pk == data.player_id:
-                        if player.pk not in new_dict:
-                            new_dict['player'] = player.pk
-                            p = re.compile('(\d*)')
-                            m = p.findall(data.GoLevel)
-                            for i in m:
-                                if i != "":
-                                    new_dict['GoLevel'] = int(i)
-            new_list.append(new_dict)
-        new_list.sort(key=lambda dictionary: dictionary['GoLevel'])
-        position = 1
-        for element in new_list:
-            element['position'] = position
-            position += 1
         return new_list
 
 
@@ -137,24 +153,8 @@ class PlayerSearch(ListView):
         elif self.search_city:
             context['query'] = urlencode({'search_city': self.search_city})
             context['search_city'] = self.search_city
-        context['rank'] = self.get_rank()
+        context['rank'] = get_rank()
         return context
-
-    @staticmethod
-    def get_rank():
-        players = Player.objects.all()
-        tournaments = Tournament.objects.order_by("date")
-        new_list = []
-        for player in players:
-            new_dict = dict()
-            for tournament in tournaments:
-                for data in tournament.playerintournament_set.all():
-                    if player.pk == data.player_id:
-                        if player not in new_dict:
-                            new_dict['player'] = player.pk
-                            new_dict['GoLevel'] = data.GoLevel
-            new_list.append(new_dict)
-        return new_list
 
 
 class CompetitorSearch(View):
