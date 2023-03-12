98fa23c0d790e205db9d43ab30ae84ab9d98c8d7
samat_sultanov <samatsayak@gmail.com>
2023-03-12 23:11:26 +0600

    создал шаблон и представление для детального просмотра ивента

--- source/webapp/views/calendar_views.py
+++ source/webapp/views/calendar_views.py
@@ -1,7 +1,20 @@
+from django.shortcuts import get_object_or_404
+
 from webapp.models import Calendar
 from webapp.forms import CalendarForm
 from django.urls import reverse
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView, TemplateView
+
+
+class CalendarDetailView(TemplateView):
+    template_name = 'calendar/calendar_view.html'
+
+    def get_context_data(self, **kwargs):
+        pk = kwargs.get('pk')
+        event = get_object_or_404(Calendar, pk=pk)
+        kwargs['event'] = event
+        return super().get_context_data(**kwargs)
+
 
 class CalendarCreateView(CreateView):
     template_name = 'calendar/calendar_create.html'
@@ -11,6 +24,7 @@ class CalendarCreateView(CreateView):
     def get_success_url(self):
         return reverse('webapp:index')
 
+
 class CalendarUpdateView(UpdateView):
     template_name = 'calendar/calendar_update.html'
     model = Calendar
@@ -18,6 +32,7 @@ class CalendarUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('webapp:index')
+
 
 class CalendarDeleteView(DeleteView):
     model = Calendar
