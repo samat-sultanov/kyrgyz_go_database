c112a7ff100345ad80cf020beeb4ea77a52140f0
samat_sultanov <samatsayak@gmail.com>
2023-03-12 18:04:05 +0600

    добавил страницу просмотра профайла тренера, создал представление и урл для него

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView
 
-# Create your views here.
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
