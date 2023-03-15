164ffd205b6e7844b64b49ff8c91ae8c405abc3c
Dastan T <dastan.turatbekov@gmail.com>
2023-03-15 12:25:26 +0600

    kgf-128/ страница удаленных ивентов реализована

--- source/webapp/views/calendar_views.py
+++ source/webapp/views/calendar_views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 
 from webapp.models import Calendar
 from webapp.forms import CalendarForm, CalendarBulkDeleteForm
@@ -47,22 +47,37 @@ class CalendarDeleteView(DeleteView):
         return HttpResponseRedirect(success_url)
 
 
-class DeletedNewsListView(FormView):
+class DeletedCalendarListView(FormView):
     form_class = CalendarBulkDeleteForm
     template_name = 'calendar/calendar_deleted_list.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['deleted_calendar_list'] = Calendar.objects.all().filter(is_deleted=True).order_by('updated_at')
+        context['deleted_calendar_list'] = Calendar.objects.all().filter(is_deleted=True).order_by('event_date')
         return context
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
-        form.fields['checkboxes'].queryset = News.objects.all().filter(is_deleted=True).order_by('updated_at')
+        form.fields['checkboxes'].queryset = Calendar.objects.all().filter(is_deleted=True).order_by('event_date')
         return form
 
     def form_valid(self, form):
-        selected_to_delete = News.objects.filter(pk__in=list(map(int, self.request.POST.getlist('checkboxes'))))
+        selected_to_delete = Calendar.objects.filter(pk__in=list(map(int, self.request.POST.getlist('checkboxes'))))
         print(selected_to_delete)
         selected_to_delete.delete()
-        return HttpResponseRedirect(reverse_lazy('webapp:deleted_news_list'))
+        return HttpResponseRedirect(reverse_lazy('webapp:deleted_calendar_list'))
+
+
+def restore_one_deleted_event(request, *args, **kwargs):
+    if request.method == 'GET':
+        event = get_object_or_404(Calendar, pk=kwargs.get('pk'))
+        event.is_deleted = False
+        event.save()
+        return redirect('webapp:deleted_calendar_list')
+
+
+def hard_delete_one_event(request, *args, **kwargs):
+    if request.method == 'POST':
+        event = get_object_or_404(Calendar, pk=kwargs.get('pk'))
+        event.delete()
+        return redirect('webapp:deleted_calendar_list')
