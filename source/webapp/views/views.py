84ee3237aa084a2dbd62955b92401b4d441b9eec
CityNomad <romancheva@hotmail.com>
2023-03-17 23:04:08 +0600

    the view for tournament was updated with new filed - information about user, who uploaded it, templates tournament_check.html and tournament_list.html were updated too

--- source/webapp/views/views.py
+++ source/webapp/views/views.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView, View
 from webapp.handle_upload import handle_uploaded_file
 from webapp.models import File, Calendar, Country, Player, Tournament, News, Game
 from webapp.forms import FileForm, CheckTournamentForm, CheckPlayerForm
+from webapp.views.functions import get_wins_losses
 
 
 def get_position_in_kgf():
@@ -81,12 +82,12 @@ def file_upload_check(request, pk):
         date = request.POST.get('date')
         regulations = request.POST.get('regulations')
         tournament_class = request.POST.get('tournament_class')
-        print(tournament_class, tournament.tournament_class)
+        uploaded_by = request.user
         if tournament_form.is_valid():
             if city == '' and date == '' and tournament_class == '' and regulations =='':
-                tournament_form = CheckTournamentForm({'city': tournament.city, 'date': tournament.date, 'tournament_class':tournament.tournament_class, 'regulations':tournament.regulations}, instance=tournament)
+                tournament_form = CheckTournamentForm({'city': tournament.city, 'date': tournament.date, 'tournament_class':tournament.tournament_class, 'regulations':tournament.regulations, 'uploaded_by': uploaded_by}, instance=tournament)
             else:
-                tournament_form = CheckTournamentForm({'city': city, 'date': date, 'tournament_class':tournament_class, 'regulations':regulations}, instance=tournament)
+                tournament_form = CheckTournamentForm({'city': city, 'date': date, 'tournament_class':tournament_class, 'regulations':regulations, 'uploaded_by': uploaded_by}, instance=tournament)
             tournament_form.save()
 
         form = CheckPlayerForm(request.POST)
@@ -112,28 +113,9 @@ def file_upload_check(request, pk):
         players = tournament.player_set.all()
         player_form = CheckPlayerForm()
         tournament_form = CheckTournamentForm()
-        games = Game.objects.filter(tournament=tournament)
-        a = []
-        for player in players:
-            new_dict = dict()
-            wins = 0
-            losses = 0
-            for game in games:
-                if game.result:
-                    if game.black == player and game.black_score > 0:
-                        wins += game.black_score
-                    elif game.white == player and game.white_score > 0:
-                        wins += game.white_score
-                    elif game.black == player and game.white_score > 0:
-                        losses += 1
-                    elif game.white == player and game.black_score > 0:
-                        losses += 1
-                new_dict['player'] = player.pk
-                new_dict['wins'] = wins
-                new_dict['losses'] = losses
-            a.append(new_dict)
+        wins = get_wins_losses(pk)
         return render(request, 'tournament/tournament_check.html',
-                      {'tournament': tournament, 'players': players, 'wins': a, 'player_form': player_form,
+                      {'tournament': tournament, 'players': players, 'wins': wins, 'player_form': player_form,
                        'tournament_form': tournament_form})
 
 
