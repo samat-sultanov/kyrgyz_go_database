60e719b5bf83ed6dd1ffc63b30b49280a01e4b4d
CityNomad <romancheva@hotmail.com>
2023-03-19 12:56:23 +0600

    working on getting a new rating

--- source/webapp/views/GoR_calculator.py
+++ source/webapp/views/GoR_calculator.py
@@ -38,4 +38,80 @@ def get_rating(pk, number):
 def get_probability_win():
     data = get_data()
     for element in data:
-        print(element)
+        new_dict = dict()
+        for key, value in element.items():
+            try:
+                if element['rating'] > element['opponent_rating']:
+                    result = element['rating'] - element['opponent_rating']
+                    new_dict['player'] = element['player']
+                    new_dict['opponent'] = element['opponent']
+                    new_dict['difference'] = result
+                    if 200 > element['rating'] >= 100:
+                        if 0 >= result > 25:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.5)
+                        elif 25 >= result > 50:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.469)
+                            print(new_rate)
+                        elif 50 >= result > 75:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.438)
+                            print(new_rate)
+                        elif 75 >= result > 100:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.407)
+                            print(new_rate)
+                        elif 100 >= result > 125:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.378)
+                            print(new_rate)
+                        elif 125 >= result > 150:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.349)
+                            print(new_rate)
+                        elif 150 >= result > 175:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.321)
+                            print(new_rate)
+                        elif 175 >= result > 200:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.294)
+                            print(new_rate)
+                        elif 200 >= result > 225:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.269)
+                            print(new_rate)
+                        elif 225 >= result > 250:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.245)
+                            print(new_rate)
+                        elif 250 >= result > 275:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.223)
+                            print(new_rate)
+                        elif 275 >= result > 300:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.202)
+                            print(new_rate)
+                        elif 300 >= result > 325:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.182)
+                            print(new_rate)
+                        elif 325 >= result > 350:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.165)
+                            print(new_rate)
+                        elif 350 >= result > 375:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.148)
+                            print(new_rate)
+                        elif 375 >= result > 400:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.133)
+                            print(new_rate)
+                        elif 400 >= result > 425:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.119)
+                            print(new_rate)
+                        elif 425 >= result > 450:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.107)
+                            print(new_rate)
+                        elif 450 >= result > 475:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.095)
+                            print(new_rate)
+                        elif 475 >= result > 500:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.085)
+                            print(new_rate)
+                        elif 500 >= result:
+                            new_rate = element['rating'] + 116 * (element['result'] - 0.076)
+                            print(new_rate)
+
+
+
+
+            except:
+                pass
