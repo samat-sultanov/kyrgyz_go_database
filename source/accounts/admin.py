3e783d2f12fb7572516694f5c76ccfcf8331aaa3
Daniiar_Abdukarimov <d.abdukarimov500@gmail.com>
2023-03-24 18:43:24 +0600

    KGF-215 На данном этапе возник баг - не хэшируется пароль. Соответственно не осуществляется вход с учетными даннми нового юзера. Разбираюсь дальше.

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+from .models import User
 
 
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(admin.ModelAdmin):
     model = User
+    fields = ['username', 'email', 'password']
 
 
 admin.site.register(User, CustomUserAdmin)
