7f7431ea822ebbd85e81c4680cf99ab71620b414
Daniiar_Abdukarimov <d.abdukarimov500@gmail.com>
2023-03-24 19:11:01 +0600

    KGF-125 Также после тестирования обнаружил, что нет номера телефона на странице тренера. Номер телефона добавил в форму.

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -10,7 +10,7 @@ class CustomUserAdmin(UserAdmin):
     add_form = UserCreationForm
     add_fieldsets = (
         (None, {
-            'fields': ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'avatar'),
+            'fields': ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'phone', 'avatar'),
         }),
     )
 
