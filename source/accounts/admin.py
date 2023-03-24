dfc9a8a9eabe27e45ebf9a84dfd1719b66d4c532
Daniiar_Abdukarimov <d.abdukarimov500@gmail.com>
2023-03-24 18:53:34 +0600

    KGF-125 Баг разрешен, сейчас юзер создается корректно. Вход осуществляется как надо - по почте либо через username.

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
-from .models import User
+User = get_user_model()
 
 
-class CustomUserAdmin(admin.ModelAdmin):
-    model = User
-    fields = ['username', 'email', 'password']
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
 
 
 admin.site.register(User, CustomUserAdmin)
