fd3b456f0ad03f0889700147804e794886a0bd03
CityNomad <romancheva@hotmail.com>
2023-03-25 16:34:51 +0600

    feature/KGF-218 the function of getting a position of a player in KGF was applied in IndexView

--- source/webapp/views/views.py
+++ source/webapp/views/views.py
@@ -11,33 +11,7 @@ from webapp.handle_upload import handle_uploaded_file
 from webapp.models import File, Calendar, Country, Player, Tournament, News, Game
 from webapp.forms import FileForm, CheckTournamentForm, CheckPlayerForm, FeedbackToEmailForm
 from webapp.views.GoR_calculator import get_new_rating
-from webapp.views.functions import get_wins_losses
-
-
-def get_position_in_kgf():
-    country = Country.objects.get(country_code='kg')
-    players = Player.objects.filter(country=country)
-    tournaments = Tournament.objects.order_by("date")
-    new_list = []
-    for player in players:
-        new_dict = dict()
-        for tournament in tournaments:
-            for data in tournament.playerintournament_set.all():
-                if player.pk == data.player_id:
-                    if player.pk not in new_dict:
-                        new_dict['player'] = player
-                        p = re.compile('(\d*)')
-                        m = p.findall(data.GoLevel)
-                        for i in m:
-                            if i != "":
-                                new_dict['GoLevel'] = int(i)
-        new_list.append(new_dict)
-    new_list.sort(key=lambda dictionary: dictionary['GoLevel'])
-    position = 1
-    for element in new_list:
-        element['position'] = position
-        position += 1
-    return new_list
+from webapp.views.functions import get_wins_losses, get_position_in_kgf
 
 
 class IndexView(TemplateView):
