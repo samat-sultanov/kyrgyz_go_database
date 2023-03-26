a62096d61daee63c0a232714f934d52ddedb956f
Daniiar_Abdukarimov <d.abdukarimov500@gmail.com>
2023-03-26 20:49:39 +0600

    feature/KGF-233 password reset sent template has been created. This template informs user that the link to reset password sent to the email.

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -10,14 +10,20 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('<int:pk>/', UserDetailView.as_view(), name='detail'),
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'),
-         name='reset_password'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset.html',
+        success_url='/accounts/reset_password_sent/'),
+        name='reset_password'),
+
     path('reset_password_sent/',
-         auth_views.PasswordResetDoneView.as_view(),
+         auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_sent.html'),
          name="password_reset_done"),
+
     path('reset/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(),
          name="password_reset_confirm"),
+
     path('reset_password_complete/',
          auth_views.PasswordResetCompleteView.as_view(),
          name="password_reset_complete"),
