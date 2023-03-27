941c1d361d13954fdd936d93b6972b6cf6240bdc
CityNomad <romancheva@hotmail.com>
2023-03-27 23:33:27 +0600

    feature/kgf-173 fixing bugs

--- source/webapp/tests.py
+++ source/webapp/tests.py
@@ -1,25 +1,27 @@
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
-
-from accounts.models import User
 from webapp.models import News
 
 
 class NewsAddTest(TestCase):
     def setUp(self):
-        self.client.login(username='admin', password='admin')
+        self.client = Client()
 
     def test_add_news(self):
         url = reverse('webapp:news_create')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
-        image_file = '/Users/nomad/Documents/3.jpeg'
-        data = {'title': 'Some text', 'text': 'Some text', 'news_image': image_file}
+        image_file = SimpleUploadedFile('image.jpg', b'image_content', content_type='image/jpg')
+        data = {'title': 'Some text',
+                'text': 'This is a long interesting text with at least 3 sentences.',
+                'news_image': image_file}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(News.objects.count(), 0)
+
+
 
 
