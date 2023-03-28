e776001edab02f6069975cfe813c7407b75eaa9a
CityNomad <romancheva@hotmail.com>
2023-03-29 00:26:18 +0600

    feature/kgf-248 tests for updating news by register user were added

--- source/webapp/tests/test_news.py
+++ source/webapp/tests/test_news.py
@@ -9,7 +9,12 @@ from accounts.models import User
 from webapp.models import News
 
 
-class NewsAddByUnregisterUserTest(TestCase):
+class NewsTestsForUnregisterUser(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_user = User.objects.create_user(username='test_user', password='test_password')
+        cls.test_news = News.objects.create(author=test_user, title='Some title', text='Some text')
+
     def setUp(self):
         self.client = Client()  # Анонимный пользователь пытается создать статью
 
@@ -24,39 +29,7 @@ class NewsAddByUnregisterUserTest(TestCase):
                 'news_image': image_file}
         response = self.client.post(url, data)  # Отправляю запрос на создание статьи
         self.assertEqual(response.status_code, 302)  # Все ещё нет доступа на страницу
-        self.assertEqual(News.objects.count(), 0)  # Проверяю, что статья не была создана
-
-
-class NewsAddByRegisterUserTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.user = User.objects.create_user(username='test_user', password='test_password')
-        News.objects.create(author=self.user, title='Some title', text='Some text')
-
-    def test_add_news_by_registered_user(self):
-        self.client.login(username='test_user', password='test_password')
-        url = reverse('webapp:news_create')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        data = {'title': 'New title',
-                'text': 'This is a long interesting text with at least 3 sentences.'}
-        response = self.client.post(url, data)
-        self.assertEqual(response.status_code, 302)
-        self.assertTrue(News.objects.filter(title='New title').exists())
-        news = News.objects.get(title='New title')
-        self.assertEqual(news.author, self.user)
-        self.assertEqual(News.objects.count(), 2)
-
-
-class NewsUpdateByUnregisterUserTest(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        test_user = User.objects.create_user(username='test_user', password='test_password')
-        cls.test_news = News.objects.create(author=test_user, title='Some title', text='Some text')
-
-    def setUp(self) -> None:
-        self.client = Client()
+        self.assertEqual(News.objects.count(), 1)  # Проверяю, что статья не была создана
 
     def test_update_news(self):
         url = reverse('webapp:news_update', args=[self.test_news.pk])
@@ -71,3 +44,39 @@ class NewsUpdateByUnregisterUserTest(TestCase):
         self.test_news.refresh_from_db()
         self.assertEqual(self.test_news.title, 'Some title')
         self.assertEqual(self.test_news.text, 'Some text')
+
+
+class NewsTestsForRegisterUser(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='test_user', password='test_password')
+        self.client.login(username='test_user', password='test_password')
+        self.news = News.objects.create(author=self.user, title='Some title', text='Some text')
+
+    def test_add_news(self):
+        url = reverse('webapp:news_create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        data = {'title': 'New title',
+                'text': 'This is a long interesting text with at least 3 sentences.'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(News.objects.filter(title='New title').exists())
+        news = News.objects.get(title='New title')
+        self.assertEqual(news.author, self.user)
+        self.assertEqual(News.objects.count(), 2)
+
+    def test_update_news(self):
+        url = reverse('webapp:news_update', args=[self.news.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        data = {'title': 'Updated title',
+                'text': 'Updated text'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('webapp:news_detail', args=[self.news.pk]))
+        news = News.objects.get(title='Updated title')
+        self.assertEqual(news.author, self.user)
+        self.assertEqual(News.objects.count(), 1)
+
