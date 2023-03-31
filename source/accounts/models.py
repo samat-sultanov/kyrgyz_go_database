12fef161666b3b94c770ee702e0914cb5da52dd6
Daniiar_Abdukarimov <d.abdukarimov500@gmail.com>
2023-03-31 15:21:29 +0600

    feature/KGF-257 email field to the User model with unique parameter added. Now emails in DB will be unique, no possibility to bind same email for two different users.

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name='email')
     avatar = models.ImageField(null=True, blank=True, upload_to='user_avatar', verbose_name="Аватар")
     phone = models.CharField(max_length=50, null=True, blank=True, verbose_name='Номер телефона')
 
