674cf9bddf2b5c73d17bdba17571087dfcd833ad
Daniiar_Abdukarimov <d.abdukarimov500@gmail.com>
2023-03-31 23:41:56 +0600

    feature/KGF-153 Change password button in base.html and standard url created. Draft password change template is also created. It should be noted that change password button may be replaced to user profile update which is not implemented at the moment.

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from accounts.views import UserDetailView
 
 app_name = 'accounts'
@@ -15,17 +15,18 @@ urlpatterns = [
         template_name='email_registration/password_reset.html',
         success_url='/accounts/reset_password_sent/'),
         name='reset_password'),
-
     path('reset_password_sent/',
          auth_views.PasswordResetDoneView.as_view(template_name='email_registration/password_reset_sent.html',),
          name="password_reset_done"),
-
     path('reset/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name='email_registration/password_reset_form.html',
                                                      success_url='/accounts/reset_password_complete/'),
          name="password_reset_confirm"),
-
     path('reset_password_complete/',
          auth_views.PasswordResetCompleteView.as_view(template_name='email_registration/password_reset_done.html'),
          name="password_reset_complete"),
+    path('change_password/',
+         PasswordChangeView.as_view(template_name='email_registration/change_password.html',
+                                    success_url='/accounts/change_password_done/'),
+         name='change_password'),
 ]
