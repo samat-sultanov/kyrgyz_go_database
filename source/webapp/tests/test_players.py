ef89c112d550f9d06ba7a1ba2cb1735743aaf665
CityNomad <romancheva@hotmail.com>
2023-04-02 00:00:25 +0600

    feature/kgf-235 finished searching competitors by rank

--- source/webapp/tests/test_players.py
+++ source/webapp/tests/test_players.py
@@ -1,31 +1,45 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from accounts.models import User
-from webapp.models import Player, Country
+from webapp.models import Player, Country, City, Club
 
 
 class PlayerTestsSearchCompetitors(TestCase):  # Для анонимных пользователей
     @classmethod
     def setUpTestData(cls):  # Создаём тестировочные данные
-        country = Country.objects.create(country_code='kg')
+        country_1 = Country.objects.create(country_code='kg')
+        country_2 = Country.objects.create(country_code='ru')
+        city_1 = City.objects.create(city="Bishkek", country=country_1)
+        city_2 = City.objects.create(city="Moskow", country=country_2)
+        club_1 = Club.objects.create(name="Sengoku")
+        club_2 = Club.objects.create(name='Spartak')
         cls.player_1 = Player.objects.create(
             first_name='Bob',
             last_name='Marley',
             current_rank='20k',
-            country=country
+            current_rating=100,
+            country=country_1,
+            city=city_1,
             )
         cls.player_2 = Player.objects.create(
             first_name='Tim',
             last_name='Dim',
             current_rank='22k',
-            country=country
+            current_rating=-100,
+            country=country_1,
+            city=city_1
         )
         cls.player_3 = Player.objects.create(
             first_name='Anna',
             last_name='Who',
             current_rank='2k',
-            country=country
+            current_rating=1900,
+            country=country_2,
+            city=city_2
         )
+        cls.player_1.clubs.add(club_1)
+        cls.player_2.clubs.add(club_1)
+        cls.player_3.clubs.add(club_2)
 
     def setUp(self):
         self.client = Client()
@@ -34,6 +48,6 @@ class PlayerTestsSearchCompetitors(TestCase):  # Для анонимных по�
         url = reverse('webapp:competitor_search') + '?search_rank=20k'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.player_1.id)
-        self.assertContains(response, self.player_2.id)
-        self.assertNotContains(response, self.player_3.id)
+        self.assertContains(response, self.player_1.last_name)
+        self.assertContains(response, self.player_2.last_name)
+        self.assertNotContains(response, self.player_3.last_name)
