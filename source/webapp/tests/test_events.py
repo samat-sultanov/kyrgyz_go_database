c6b8479128745ba64a00d00077d98d496918f886
CityNomad <romancheva@hotmail.com>
2023-04-03 17:29:05 +0600

    bug/kgf-225 tests for soft deleting events were successfully changed

--- source/webapp/tests/test_events.py
+++ source/webapp/tests/test_events.py
@@ -1,4 +1,6 @@
 import datetime
+
+from django.contrib.auth.models import Permission
 from django.test import TestCase, Client
 from django.urls import reverse
 from accounts.models import User
@@ -10,15 +12,17 @@ class EventCreateTestByRegisteredUser(TestCase):
     def setUpTestData(cls):
         cls.test_admin = User.objects.create_superuser(username='admin', email='admin@admin.com', password='admin')
         cls.test_user = User.objects.create_user(username='test_user', email='test@user.com', password='test_user')
+        cls.delete_event_permission = Permission.objects.get(codename='delete_calendar')
+        cls.test_user.user_permissions.add(cls.delete_event_permission)
         country = Country.objects.create(country_code='kg')
         cls.city_1 = City.objects.create(city='Bishkek', country=country)
         cls.city_2 = City.objects.create(city='Talas', country=country)
 
     def setUp(self):
         self.client = Client()
-        self.client.login(username='test_user', password='test_user')
 
     def test_view_create_event(self):
+        self.client.login(username='test_user', password='test_user')
         url = reverse('webapp:event_create')
         data = {
             'event_name': 'test_event_name',
@@ -40,6 +44,7 @@ class EventCreateTestByRegisteredUser(TestCase):
         self.assertEqual(event.deadline, datetime.date(2024, 10, 10))
 
     def test_update_event(self):
+        self.client.login(username='test_user', password='test_user')
         new_event = Calendar.objects.create(event_name='New event',
                                             text='New text',
                                             event_city=self.city_1,
@@ -65,6 +70,24 @@ class EventCreateTestByRegisteredUser(TestCase):
         self.assertEqual(updated_event.deadline, datetime.date(2030, 12, 12))
         self.assertEqual(updated_event.text, 'Updated text')
         self.assertEqual(updated_event.event_city, self.city_2.city)
+
+    def test_soft_delete_event(self):
+        self.client.login(username='test_user', password='test_user')
+        new_event = Calendar.objects.create(event_name='New event',
+                                            text='New text',
+                                            event_city=self.city_1,
+                                            event_date=datetime.date(2030, 11, 11),
+                                            deadline=datetime.date(2030, 11, 11))
+        url = reverse('webapp:index')
+        response = self.client.get(url)
+        self.assertContains(response, 'data-bs-target="#delete_event_Modal"')
+        response = self.client.post(reverse('webapp:event_delete', args=[new_event.pk]))
+        new_event.refresh_from_db()
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Calendar.objects.filter(pk=new_event.pk).exists())
+        self.assertEqual(new_event.is_deleted, True)
+        self.assertEqual(Calendar.objects.count(), 1)
+        self.assertEqual(new_event.event_name, "New event")
 
     def tearDown(self) -> None:
         self.client.logout()
