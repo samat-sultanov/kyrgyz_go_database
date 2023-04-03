eb1deb8ea6fe56ad2e1e60df965fa0cb11b89d9a
CityNomad <romancheva@hotmail.com>
2023-04-03 16:59:44 +0600

    bug/kgf-225 tests for updating existing event by register user were added successfully

--- source/webapp/tests/test_events.py
+++ source/webapp/tests/test_events.py
@@ -1,12 +1,11 @@
 import datetime
-
 from django.test import TestCase, Client
 from django.urls import reverse
 from accounts.models import User
 from webapp.models import Calendar, Country, City
 
 
-class EventCreateTest(TestCase):
+class EventCreateTestByRegisteredUser(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.test_admin = User.objects.create_superuser(username='admin', email='admin@admin.com', password='admin')
@@ -17,9 +16,9 @@ class EventCreateTest(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.client.login(username='test_user', password='test_user')
 
     def test_view_create_event(self):
-        self.client.login(username='test_user', password='test_user')
         url = reverse('webapp:event_create')
         data = {
             'event_name': 'test_event_name',
@@ -39,6 +38,36 @@ class EventCreateTest(TestCase):
         self.assertEqual(event.event_date, datetime.date(2024, 10, 10))
         self.assertEqual(event.text, 'Test text for event create test')
         self.assertEqual(event.deadline, datetime.date(2024, 10, 10))
+
+    def test_update_event(self):
+        new_event = Calendar.objects.create(event_name='New event',
+                                            text='New text',
+                                            event_city=self.city_1,
+                                            event_date=datetime.date(2030, 11, 11),
+                                            deadline=datetime.date(2030, 11, 11))
+        url = reverse('webapp:event_update', args=[new_event.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        data = {
+            'event_name': 'Updated event',
+            'event_city': self.city_2,
+            'event_date': datetime.date(2030, 12, 12),
+            'text': 'Updated text',
+            'deadline': datetime.date(2030, 12, 12),
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+        redirect_url = reverse('webapp:index')
+        self.assertRedirects(response, redirect_url)
+        updated_event = Calendar.objects.get(pk=new_event.pk)
+        self.assertEqual(updated_event.event_name, 'Updated event')
+        self.assertEqual(updated_event.event_date, datetime.date(2030, 12, 12))
+        self.assertEqual(updated_event.deadline, datetime.date(2030, 12, 12))
+        self.assertEqual(updated_event.text, 'Updated text')
+        self.assertEqual(updated_event.event_city, self.city_2.city)
+
+    def tearDown(self) -> None:
+        self.client.logout()
 
 
 #
