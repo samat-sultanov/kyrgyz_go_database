e85499012f471d4c5f16a9669801a0c651042330
Akramerwin <akram.beat77@gmail.com>
2023-04-07 18:28:32 +0600

    Написал успешнный тест на поиск игрока

--- source/webapp/tests/test_search_players.py
+++ source/webapp/tests/test_search_players.py
@@ -1,73 +1,65 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from accounts.models import User
-from webapp.models import Player, City, Country
+from webapp.models import Player, City, Country, Club
 
 
-class TournamentTestsForUnregisterUser(TestCase):  # Для анонимных пользователейзне
+class PlayerTestsForSearch(TestCase):
     @classmethod
     def setUpTestData(cls):  # Создаём тестировочные данные
-        test_user = User.objects.create_user(username='test_user', password='test_password')
-        country = Player.objects.create(country_code='kg')
-        city_1 = City.objects.create(city='Test_city', country=country)
-        cls.test_tournament_1 = Tournament.objects.create(
-            uploaded_by=test_user,
-            name='Some name',
+        country = Country.objects.create(country_code='kg')
+        city_1 = City.objects.create(city='Bishkek', country=country)
+        city_2 = City.objects.create(city='Karakol', country=country)
+        club_1 = Club.objects.create(name='sengoku')
+        club_2 = Club.objects.create(name='aden')
+        cls.test_player_1 = Player.objects.create(
+            first_name='Akram',
+            last_name='Begmetov',
+            country=country,
             city=city_1,
-            tournament_class='A',
-            date='2023-03-03',
-            rounds=7)
-        cls.test_tournament_2 = Tournament.objects.create(
-            uploaded_by=test_user,
-            name='Tournament',
+            birth_date='2023-03-03',
+            current_rank='20',
+            current_rating='30')
+        cls.test_player_1.clubs.set([club_1])
+        cls.test_player_2 = Player.objects.create(
+            first_name='Alex',
+            last_name='Di',
+            country=country,
             city=city_2,
-            tournament_class='B',
-            date='2022-01-20',
-            rounds=9)
+            birth_date='2023-03-03',
+            current_rank='20',
+            current_rating='30')
+        cls.test_player_2.clubs.set([club_2])
 
     def setUp(self):
         self.client = Client()
 
-    def test_search_by_name(self):
-        url = reverse('webapp:tournament_search') + '?search_name=S'
+    def test_search_by_last_name(self):
+        url = reverse('webapp:player_search') + '?search_last_name=B'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.test_tournament_1.name)
-        self.assertNotContains(response, self.test_tournament_2.name)
+        self.assertContains(response, self.test_player_1.first_name)
+        self.assertNotContains(response, self.test_player_2.first_name)
 
-    def test_search_by_class(self):
-        url = reverse('webapp:tournament_search') + '?search_tournament_class=A'
+    def test_search_by_name(self):
+        url = reverse('webapp:player_search') + '?search_first_name=Ak'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.test_tournament_1.name)
-        self.assertNotContains(response, self.test_tournament_2.name)
+        self.assertContains(response, self.test_player_1.first_name)
+        self.assertNotContains(response, self.test_player_2.first_name)
+
+    def test_search_by_clubs(self):
+        url = reverse('webapp:player_search') + '?search_clubs=s'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.test_player_1.first_name)
+        self.assertNotContains(response, self.test_player_2.first_name)
 
     def test_search_by_city(self):
-        url = reverse('webapp:tournament_search') + '?search_city=B'
+        url = reverse('webapp:player_search') + '?search_city=B'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.test_tournament_1.name)
-        self.assertNotContains(response, self.test_tournament_2.name)
+        self.assertContains(response, self.test_player_1.first_name)
+        self.assertNotContains(response, self.test_player_2.first_name)
 
-    def test_search_by_date(self):
-        url = reverse('webapp:tournament_search') + '?search_date=2023-03-03'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.test_tournament_1.name)
-        self.assertNotContains(response, self.test_tournament_2.name)
 
-    def test_search_all_fields(self):
-        url = reverse('webapp:tournament_search') + \
-              '?search_name=S&search_city=B&search_date=2023-03-03&search_tournament_class=A'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.test_tournament_1.name)
-        self.assertNotContains(response, self.test_tournament_2.name)
-
-    def test_search_null_fields(self):
-        url = reverse('webapp:tournament_search') + \
-              '?search_name=&search_city=&search_date=&search_tournament_class='
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.test_tournament_1.name)
-        self.assertContains(response, self.test_tournament_2.name)
