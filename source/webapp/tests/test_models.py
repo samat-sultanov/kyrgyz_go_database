a7d65b6d92072a66fc2581e02637d0e023153290
samat_sultanov <samatsayak@gmail.com>
2023-04-30 23:01:27 +0600

    feature/kgf-358 написал тесты на модель Region

--- source/webapp/tests/test_models.py
+++ source/webapp/tests/test_models.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 import accounts.models
+from django.core.exceptions import ValidationError
 from accounts.models import User
-from webapp.models import Recommendation, Player, Country
+from webapp.models import Recommendation, Player, Country, Region
 import time
 
 
@@ -65,3 +66,44 @@ class RecommendationModelTest(TestCase):
     def test_related_name_parameter(self):
         self.assertIn(self.recommendation, self.user.author.all())
         self.assertIn(self.recommendation, self.player.player.all())
+
+
+class RegionModelTest(TestCase):
+    def test_create_region_with_correct_parameters(self):
+        country = Country.objects.create(country_code='kg')
+        Region.objects.create(name='Chuy', country=country)
+        self.assertEqual(len(Region.objects.all()), 1)
+        created_region = Region.objects.filter(name='Chuy').first()
+        self.assertEqual(created_region.name, 'Chuy')
+        self.assertEqual(created_region.country, country)
+
+    def test_create_region_with_wrong_parameters(self):
+        country = Country.objects.create(country_code='US')
+        region = Region(country=country, name='')
+        with self.assertRaises(ValidationError):
+            region.full_clean()
+            region.save()
+        self.assertEqual(len(Region.objects.all()), 0)
+
+    def test_update_region(self):
+        country = Country.objects.create(country_code='kg')
+        region = Region.objects.create(name='Chuy', country=country)
+        region.name = 'Naryn'
+        region.save()
+        updated_region = Region.objects.first()
+        self.assertEqual(updated_region.name, 'Naryn')
+
+    def test_delete_region(self):
+        country = Country.objects.create(country_code='kg')
+        region = Region.objects.create(name='Chuy', country=country)
+        self.assertEqual(len(Region.objects.all()), 1)
+        region.delete()
+        self.assertEqual(len(Region.objects.all()), 0)
+
+
+
+
+
+
+
+
